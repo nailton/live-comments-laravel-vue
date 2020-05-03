5e3a43a4ebfd16d42980bf2853d5e391ac7dcb6a
Nailton Sousa <nailton.osn@gmail.com>
2020-05-03 17:01:46 -0300

    NewComment template page

---
+++ resources/js/components/NewComment.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="commentForm" class="box has-shadow has-background-white">
+        <form @keyup.enter="postComment">
+            <div class="field has-margin-top">
+                <label class="label">Seu nome</label>
+                <div class="control">
+                    <input type="text" v-model="comment.author" class="input is-medium" placeholder="Seu nome">
+                </div>
+            </div>
+            <div class="field has-margin-top">
+                <label class="label">Seu comentário</label>
+                <div class="control">
+                    <textarea style="height: 100px;" name="comment" class="input is-medium" autocomplete="true" v-model="comment.content" placeholder="lorem ipsum"></textarea>
+                </div>
+            </div>
+            <div class="control has-margin-top">
+                <button style="background-color: #47b784" :class="{'is-loading': submit}" class="button has-shadow is-medium has-text-white" :disabled="!isValid" @click.prevent="postComment" type="submit"></button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "NewComment",
+    data() {
+        return {
+            submit: false,
+            comment: {
+                content: '',
+                author: '',
+            }
+        }
+    },
+    methods: {
+        postComment() {
+            this.submit = true;
+            this.$store.dispatch('ADD_COMMENT', this.comment)
+            this.submit = false;
+            if (response.data === 'ok')
+                console.log('success')
+                .catch(err => {
+                    this.submit = false
+                })
+        }
+    },
+    computed: {
+        isValid() {
+            return this.comment.content !== '' && this.comment.author !== ''
+        }
+    }
+}
+
+</script>
+<style scoped>
+.has-margin-top {
+    margin-top: 15px;
+}
+</style>
